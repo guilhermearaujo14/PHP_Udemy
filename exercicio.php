0929b298800a35571edfb2bbe296f75668fcfbaa
Guilherme Araujo <40188385+guilhermearaujo14@users.noreply.github.com>
2020-08-24 23:59:32 -0300

    Add files via upload

---
+++ exercicio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Exercicio</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualizaçao dos Exercicios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+           
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R E ALUNOS <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
